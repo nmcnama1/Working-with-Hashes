c523eff6418b78508cb4691a936d0ee1c97aeeb4
Jonathan Arp <jarp@nd.edu>
2016-11-01 13:21:29 -0400

    notes

--- run.rb
+++ run.rb
@@ -9,14 +9,23 @@ end
 
 get '/states' do
   # code!
+
+  #jarp this works but can be done much simpler...
   @states = []
-  @state = {:id=>'IN', :name=>'Indiana'}
-  @state1 = {:id=>'MO', :name=>'Missouri'}
-  @state2 = {:id=>'CA', :name=>'California'}
-  @state3 = {:id=>'FL', :name=>'Florida'}
-  @state4 = {:id=>'TN', :name=>'Tennessee'}
-  @state5 = {:id=>'TX', :name=>'Texas'}
-  @states.push(@state, @state1, @state2, @state3, @state4, @state5)
+  @states << {:id=>'IN', :name=>'Indiana'}
+  @states << {:id=>'MO', :name=>'Missouri'}
+  @states << {:id=>'CA', :name=>'California'}
+  @states << {:id=>'FL', :name=>'Florida'}
+  @states << {:id=>'TN', :name=>'Tennessee'}
+  @states << {:id=>'TX', :name=>'Texas'}
+
+  # @state = {:id=>'IN', :name=>'Indiana'}
+  # @state1 = {:id=>'MO', :name=>'Missouri'}
+  # @state2 = {:id=>'CA', :name=>'California'}
+  # @state3 = {:id=>'FL', :name=>'Florida'}
+  # @state4 = {:id=>'TN', :name=>'Tennessee'}
+  # @state5 = {:id=>'TX', :name=>'Texas'}
+  # @states.push(@state, @state1, @state2, @state3, @state4, @state5)
   @states.sort_by! {|state| state[:name]}
   erb :states, layout: :main
 end
